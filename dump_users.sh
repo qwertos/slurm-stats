862f576fb2574b085355d3135c970114675a5cf2
Aaron Herting <qwertos@river.rit.edu>
2013-06-12 15:29:07 -0400

    added lock file

--- dump_users.sh
+++ dump_users.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 
+# Author: Aaron Herting
+# License: MIT
+
+
+LOCK_FILE="/var/lock/slurmplot/$BASHPID"
+
+mkdir -p /var/lock/slurmplot
+
+touch $LOCK_FILE
+
 cat | awk -F'\t' '{ print $2 ; }' | sort | uniq 
 
+
+rm -f $LOCK_FILE
 

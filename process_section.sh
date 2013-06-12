3ff6e1cadf00667cd2bd9b9161428a9d76b2c5b6
Aaron Herting <qwertos@river.rit.edu>
2013-06-11 20:43:50 -0400

    changed cut to awk... slightly better speed...

--- process_section.sh
+++ process_section.sh
@@ -4,13 +4,13 @@
 # License: MIT
 
 while read line ; do
-	JOB_ID=`echo "$line" | cut -d'|' -f1`
-	PARTITION=`echo "$line" | cut -d'|' -f2`
-	USERNAME=`echo "$line" | cut -d'|' -f3`
-	ELAPSED=`echo "$line" | cut -d'|' -f4`
-	SUBMIT=`echo "$line" | cut -d'|' -f5`
-	START=`echo "$line" | cut -d'|' -f6`
-	END=`echo "$line" | cut -d'|' -f7`
+	JOB_ID=`echo "$line" | awk -F'|' '{ print $1 ; }'`
+	PARTITION=`echo "$line" | awk -F'|' '{ print $2 ; }'`
+	USERNAME=`echo "$line" | awk -F'|' '{ print $3 ; }'`
+	ELAPSED=`echo "$line" | awk -F'|' '{ print $4 ; }'`
+	SUBMIT=`echo "$line" | awk -F'|' '{ print $5 ; }'`
+	START=`echo "$line" | awk -F'|' '{print $6 ; }'`
+	END=`echo "$line" | awk -F'|' '{print $7 ; }'`
 	
 	U_SUBMIT=`echo "$SUBMIT" | date --file - '+%s'`
 	U_START=`echo "$START" | date --file - '+%s'`
